53c8e61e393bc8f429ef3b0af7369cfbb58a4758
caramina <s10256038@connect.np.edu.sg>
2023-08-01 13:04:45 +0800

    commit .py file

--- mainSolution.py
+++ mainSolution.py
@@ -1,1 +1,12 @@
-# hello
+from pathlib import Path
+import csv
+
+# create a file to csv file.
+fp = Path.cwd()/"cash-on-hand.csv"
+fp = Path.cwd()/"operating expenses.csv"
+fp = Path.cwd()/"profits and loss.csv"
+
+# read the csv file to append profit and quantity from the csv.
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    next(reader) # skip header
