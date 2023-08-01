d35265345dd5cf4ff9a7990ec570186adbf33946
ch3rrys <s10258721@connect.np.edu.sg>
2023-08-01 12:24:32 +0800

    importing csv file

--- mainSolution.py
+++ mainSolution.py
@@ -1,1 +1,10 @@
-# hello
+from pathlib import Path
+import csv
+
+# create a file to csv file.
+fp = Path.cwd()/"TimeSheetAndSales.csv"
+
+# read the csv file to append profit and quantity from the csv.
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    next(reader) # skip header
