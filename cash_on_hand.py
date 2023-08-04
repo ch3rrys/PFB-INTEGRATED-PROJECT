59b3b2591798ce775a90bff1cd738ea6fea33d80
yanfangg <S10260001@connect.np.edu.sg>
2023-08-04 13:17:23 +0800

    trying to print

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,17 +1,20 @@
 from pathlib import Path
 import csv
 
-# create a file to csv file.
-fp = Path.cwd()/"csv_reports/cash_on_hand.csv"
+def calculate_cash_on_hand():
 
-# read the csv file to append profit and quantity from the csv.
-with fp.open(mode="r", encoding="UTF-8", newline="") as file:
-    reader = csv.reader(file)
-    next(reader) # skip header
+    # create a file to csv file.
+    fp = Path.cwd()/"csv_reports/cash_on_hand.csv"
 
-    cash_on_hand=[]
+    # read the csv file to append profit and quantity from the csv.
+    with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+        reader = csv.reader(file)
+        next(reader) # skip header
 
-    for row in reader:
-        cash_on_hand.append([row[0],row[1]])  
+        cash_on_hand=[]
 
-    print(cash_on_hand)
+        for row in reader:
+            cash_on_hand.append([row[0],row[1]])
+        
+        return cash_on_hand
+print(calculate_cash_on_hand())
--- profits_loss.py
+++ profits_loss.py
@@ -1,16 +1,19 @@
 from pathlib import Path
 import csv
 
-# create a file to csv file.
-fp_3 = Path.cwd()/"csv_reports/profits_and_loss.csv"
+def calculate_profit_n_loss():
 
-with fp_3.open(mode="r", encoding="UTF-8", newline="") as file:
-    reader = csv.reader(file)
-    next(reader) # skip header
+    # create a file to csv file.
+    fp_3 = Path.cwd()/"csv_reports/profits_and_loss.csv"
 
-    profit_n_loss=[]
+    with fp_3.open(mode="r", encoding="UTF-8", newline="") as file:
+        reader = csv.reader(file)
+        next(reader) # skip header
 
-    for row in reader:
-        profit_n_loss.append([row[0],row[1],row[2],row[3],row[4]])  
+        profit_n_loss=[]
 
-    print(profit_n_loss)
+        for row in reader:
+            profit_n_loss.append([row[0],row[1],row[2],row[3],row[4]])  
+
+        return profit_n_loss
+print(calculate_profit_n_loss())
