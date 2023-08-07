aa34d019a495d6493786e19ef373dd31ba5d4638
brendahaha <S10257382@connect.np.edu.sg>
2023-08-07 15:58:31 +0800

    added net profit surplus

--- profits_loss.py
+++ profits_loss.py
@@ -15,6 +15,12 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     for row in reader:
         net_profit_data.append([int(row[0]), int(row[4])])
 
+# Calculate the difference in net profit between each day and the previous day
+differences = [net_profit_data[i][1] - net_profit_data[i - 1][1] for i in range(1, len(net_profit_data))]
+
+# Calculate the highest net profit surplus
+highest_surplus = max(differences)
+
 # Initialize a list to store the cash deficit values
 cash_deficits = []
 
@@ -28,6 +34,13 @@ for i in range(1, len(net_profit_data)):
         cash_deficits.append([net_profit_data[i][0], difference])
 
 # Print cash deficit results
+#print results; if deficit list is empty (no deficit) we print cash surplus
+if not cash_deficits:
+    print("[NET PROFIT HIGHEST SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+    print(f"[HIGHEST CASH SURPLUS] DAY: {highest_surplus[0]}, AMOUNT: USD{highest_surplus[1]}")
+else:
+    for deficit in cash_deficits:
+         print(f"[CASH DEFICIT] DAY: {deficit[0]}, AMOUNT: USD{deficit[1]}")
 if cash_deficits:
     for deficit in cash_deficits:
         print(f"[CASH DEFICIT] DAY: {deficit[0]}, AMOUNT: USD{deficit[1]}")
