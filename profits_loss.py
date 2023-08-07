1a6925ea7ba1e7e2e20397662105c0d0ad6402f4
ch3rrys <s10258721@connect.np.edu.sg>
2023-08-07 15:22:34 +0800

    imported file, calculated diff in net profits

--- profits_loss.py
+++ profits_loss.py
@@ -1,19 +1,31 @@
 from pathlib import Path
 import csv
 
-def calculate_profit_n_loss():
+# Create a file path to the CSV file
+fp = Path.cwd() / "csv_reports/profits_and_loss.csv"
 
-    # create a file to csv file.
-    fp_3 = Path.cwd()/"csv_reports/profits_and_loss.csv"
+# Initialize a list to store all the values of net profit
+net_profit_data = []
 
-    with fp_3.open(mode="r", encoding="UTF-8", newline="") as file:
-        reader = csv.reader(file)
-        next(reader) # skip header
+# Read the CSV file to append "Day" and "Net Profit" values
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    next(reader)  # Skip header
 
-        profit_n_loss=[]
+    for row in reader:
+        net_profit_data.append([int(row[0]), int(row[4])])
 
-        for row in reader:
-            profit_n_loss.append([row[0],row[1],row[2],row[3],row[4]])  
+# Initialize a list to store the differences in net profit
+profit_differences = []
 
-        return profit_n_loss
-print(calculate_profit_n_loss())
+# Initialize variables to keep track of the highest increment
+highest_increment_day = None
+highest_increment_amount = 0
+
+# Iterate through each list object to compare differences in net profit
+for i in range(1, len(net_profit_data)):
+    # Calculate the difference in net profit between the current and previous day
+    difference = net_profit_data[i][1] - net_profit_data[i - 1][1]
+    
+    # Append the difference to the list
+    profit_differences.append([net_profit_data[i][0], difference])
