5405bbbc8cf08bb6490c80d03a80a1dc6bc4dd4a
brendahaha <S10257382@connect.np.edu.sg>
2023-08-07 15:16:39 +0800

    import files,  iterate through each list object to compare difference in cash

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,20 +1,36 @@
 from pathlib import Path
 import csv
 
-def calculate_cash_on_hand():
+# create a file to csv file.
+fp = Path.cwd()/"csv_reports/cash_on_hand.csv"
 
-    # create a file to csv file.
-    fp = Path.cwd()/"csv_reports/cash_on_hand.csv"
+#initialize list to store all the values of cash on hand
+cash_on_hand=[]
 
-    # read the csv file to append profit and quantity from the csv.
-    with fp.open(mode="r", encoding="UTF-8", newline="") as file:
-        reader = csv.reader(file)
-        next(reader) # skip header
+# read the csv file to append profit and quantity from the csv.
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    next(reader) # skip header
 
-        cash_on_hand=[]
+    for row in reader:
+        cash_on_hand.append([int(row[0]), int(row[1])])
 
-        for row in reader:
-            cash_on_hand.append([row[0],row[1]])
-        
-        return cash_on_hand
-print(calculate_cash_on_hand())
+#initialize list to store values for cash deficit days
+cash_deficit = []
+
+#initialize list to store highest cash surplus, we put day 0 values in first
+highest_surplus = [cash_on_hand[0][0], 0]
+
+#iterate through each list object to compare difference in cash
+for i in range(1, len(cash_on_hand)):
+    # Calculate the surplus as the difference between current and previous cash values
+    surplus = cash_on_hand[i][1] - cash_on_hand[i - 1][1]
+    
+    if surplus > 0:
+        # Check if surplus is higher than the highest surplus
+        if surplus > highest_surplus[1]:
+            highest_surplus[0] = cash_on_hand[i][0]
+            highest_surplus[1] = surplus
+    else:
+        #append deficit to deficit list
+        cash_deficit.append([cash_on_hand[i][0], cash_on_hand[i][1]])
