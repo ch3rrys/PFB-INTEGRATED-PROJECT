d12b4ce2054f7762ff488928e2e13ec350928e58
caramina <s10256038@connect.np.edu.sg>
2023-08-10 14:48:57 +0800

    added comments for code

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from cash_on_hand import calculate_cash_on_hand
 from overheads import calculate_overheads
 from profits_loss import calculate_profit_loss
 
+# calculate the cash on hand, overheads, profit and loss using the appropriate function
 cash_output = calculate_cash_on_hand()
 overheads_output = calculate_overheads()
 profit_loss_output = calculate_profit_loss()
 
+# open a file named "summary_report.txt" in write mode
 with open("summary_report.txt", "w") as file:
     file.write("Cash On Hand\n==========================\n")
     file.write(cash_output)
